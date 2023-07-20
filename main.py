dae7f8d3cae7e5e7f33b2528fe96ae32fd6a8115
Powie <PowieIsCool@gmail.com>
2023-07-20 16:52:30 +0800

    it kinda works now

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from time import sleep
-from math import random
+from random import randint, seed
 
 Min = 1
 Max = 9
 
-function setRange(minn,maxx):
 
+def setRange(min=1,max=9):
+    Min = min
+    Max = max
+    print("")
+
+def showControls():
+    print("Controls:")
+    print('"S" = start')
+    # print('"S" = start')
+    # print('"S" = start')
+
+def makeAnser():
+    first = randint(Min,Max)
+    second = randint(Min,Max)
+    print(f"{first} + {second}")
+    return first + second
 
 while True:
-    Input = input()
+    print("Halo~")
+    showControls()
+    Input = input().lower()
+    if Input == "s":
+        while True:
+            Ans = makeAnser()
+            Input = input().lower()
+            print(f"debug: {Ans}")
+            if Input == "c":
+                break
+            elif Input == str(Ans):
+                print("Nice")
+            else:
+                print("wrong mf")
+            sleep(.3)
+
+    
+    
     sleep(1)
