6189d1b4803b1c3b408932f30028ee5ebc1e6ea6
Powie <PowieIsCool@gmail.com>
2023-08-09 13:15:00 +0800

    idk man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from random import randint, seed
+import msvcrt
 
 Min = 1
 Max = 9
@@ -13,7 +14,7 @@ def setRange(min=1,max=9):
 def showControls():
     print("Controls:")
     print('"S" = start')
-    # print('"S" = start')
+    print('"range" = set the range of numbers')
     # print('"S" = start')
 
 def makeAnser():
@@ -25,18 +26,18 @@ def makeAnser():
 while True:
     print("Halo~")
     showControls()
-    Input = input().lower()
+    Input =  msvcrt.getch()
     if Input == "s":
         while True:
             Ans = makeAnser()
             Input = input().lower()
-            print(f"debug: {Ans}")
             if Input == "c":
                 break
             elif Input == str(Ans):
                 print("Nice")
             else:
                 print("wrong mf")
+                print(f"Right: {Ans}")
             sleep(.3)
 
     
