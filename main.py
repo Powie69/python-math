80b09e73cc158d57c0c2e1713db66b83ec44b5d5
Powie <PowieIsCool@gmail.com>
2023-08-17 12:30:47 +0800

    added seeding

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ colorama.init()
 Min = 1
 Max = 9
 
+Seed = 69420
+seedToggled = False
 
 def setRange(min=1,max=9):
     Min = min
@@ -18,6 +20,8 @@ def showControls():
     print("Controls:")
     print('"S" = start')
     print('"range" = set the range of numbers')
+    print('"ts" = toggle seeding')
+    print('"ss" = set seed')
     # print('"S" = start')
 
 def makeAnser():
@@ -26,10 +30,24 @@ def makeAnser():
     print(f"{first} + {second}")
     return first + second
 
+def toggleSeed():
+    global seedToggled
+    if seedToggled == True:
+        seed(randint(9,32767))
+        print("turning off seed")
+    else:
+        seed(Seed)
+        seedToggled = True
+        print("turning on seed")
+
+def setSeed():
+    Input = input("enter seed: ")
+    Seed = Input
+
 while True:
     print(f"{Fore.GREEN}{Style.BRIGHT}Halo~")
     showControls()
-    Input = input()
+    Input = input().lower()
     if Input == "s":
         while True:
             Ans = makeAnser()
@@ -42,6 +60,10 @@ while True:
                 print(f"{Fore.RED}wrong mf")
                 print(f"Right: {Ans}")
             sleep(.3)
+    elif Input == "ts":
+        toggleSeed()
+    elif Input == "ss":
+        setSeed()        
 
     
     
