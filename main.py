b45c3ffc5467956c47c000bb7abaade1a79fdd9b
Powie <PowieIsCool@gmail.com>
2025-01-03 15:02:42 +0800

    updates updates updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import sleep
-from random import randint, seed
+from random import randint, seed, shuffle
 from colorama import Fore, Style
 import colorama
 
@@ -12,57 +12,61 @@ seedToggled = False
 isNumSet = False
 numSet = None
 
-def setNum():
+def setNum(Args):
 	global isNumSet, numSet
-	if isNumSet == True:
+	if len(Args) <= 1:
 		isNumSet = False
-		print("Turning off setNum")
-	else:
-		Input = input("Number: ")
-		if not Input.isdigit():
-			return print('not a number')
-		isNumSet = True
-		numSet = int(Input)
-		print(f"Turning on setNum: {numSet}")
+		return print("setNum disabled")
+	if not Args[1].isdigit():
+		return print('not a number')
+	isNumSet = True
+	numSet = int(Args[1])
+	print(f"Turning on setNum: {numSet}")
 
-def	setRange(min=1,max=9):
-	Min = min
-	Max = max
-	print("")
+def	setRange(Args):
+	if len(Args) < 3:
+		return
+	global Min, Max
+	try:
+		Min = int(Args[1])
+		Max = int(Args[2])
+	except ValueError:
+		print(f'{Fore.RED}Arguments must be Integers{Fore.RESET}')
+	print(f"Min: {Args[1]}, Max: {Args[2]}")
 
 def showControls():
 	print("Controls:")
 	print('"S" = start')
 	print('"range" = set the range of numbers')
 	print('"ts" = toggle seeding')
+	print('"sn" = set number')
 
 def makeAnser():
-	if isNumSet == True:
-		first = numSet
-	else:
-		first = randint(Min,Max)
-	second = randint(Min,Max)
-	print(f"{first} + {second}")
-	return first + second
+	digits = [numSet, randint(Min, Max)] if isNumSet else [randint(Min, Max), randint(Min, Max)]
+	shuffle(digits)
+	print(f"{digits[0]} + {digits[1]}")
+	return sum(digits)
 
-def toggleSeed():
+def toggleSeed(Args):
 	global seedToggled
-	if seedToggled == True:
+	if len(Args) <= 1:
+		print('setting random seed')
 		seed(randint(9,32767))
-		seedToggled = False
-		print("turning off seed")
-	else:
-		Input = input("enter seed: ")
-		seed(Input)
-		print("turning on seed")
+		return
+	if not Args[1].isdigit():
+		return print('not a number!')
+	seed(Args[1])
+	print('')
 
 
 showControls()
 
 while True:
 	print(f"{Fore.GREEN}{Style.BRIGHT}Halo~")
-	Input = input().lower()
-	if Input == "s":
+	Input = input().lower().split()
+	command = Input[0]
+	print(Input)
+	if command == "s":
 		# game loop
 		while True:
 			Ans = makeAnser()
@@ -75,14 +79,12 @@ while True:
 				print(f"{Fore.RED}wrong mf")
 				print(f"Right: {Ans}")
 			sleep(.3)
-	elif Input == "range":
-		minInput = input("enter minimum: ")
-		maxInput = input("enter maximum: ")
-		setRange(minInput,maxInput)
-	elif Input == "ts":
-		toggleSeed()
-	elif Input == "sn":
-		setNum()
+	elif command == "range":
+		setRange(Input)
+	elif command == "ts":
+		toggleSeed(Input)
+	elif command == "sn":
+		setNum(Input)
 	else:
 		print("what u talkin' about?")
-	sleep(1)
+	sleep(.5)
